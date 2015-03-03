e157744050e63c048948dfc7557fa88a58666232
jeffkoop <jeff.koop@gmail.com>
2015-03-03 13:10:40 +0100

    removed date() for copyright
    because of error message on the webserver.

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,10 @@
 <?php
-$this_Year = date("Y");
 define("COMPANY_NAME", "FacebookGraph");
 ?>
 
 <footer>
     <div class="container">
-        <h6>Copyright &copy; <?php echo $this_Year ?> <?php echo COMPANY_NAME; ?></h6>
+        <h6>Copyright &copy; 2015 <?php echo COMPANY_NAME; ?></h6>
     </div>      
 </footer>
 
