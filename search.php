0c58b8151d9a90aad776036258ef0a7eb41e4a73
Joost96 <joost-k@live.nl>
2015-03-04 22:56:26 +0100

    added loading icon for loading the results
    also made a mess in search.php , sorry :(

--- search.php
+++ search.php
@@ -11,11 +11,37 @@ $page = 'search';
         <br>
         <br>
         <br>
-        <br>
-        
+		<div class="container">
+			<form action="search.php">
+				<div class="input-group ">
+					<input type="text" class="form-control" name="search" placeholder="Search for..." value="<?php if(isset($_GET['search'])){echo($_GET['search']);};?>">
+					<span class="input-group-btn">
+						<button class="submit btn btn-default" type="submit" id="search">Search</button>
+					</span>
+				</div>
+			</form>
+			<?php
+			if(isset($_GET['search']) && !empty($_GET['search'])) {?>
+			<img id="loading" alt="" src="images/loading.gif"/>
+			<div id="result">
+			</div>
+			<br>
+			<?php
+			}
+			?>
+        </div>
         <?php include('includes/footer.php'); ?>
         <?php include('includes/includes_bot.php'); ?>
-
+		
     </body>
+	<script type="text/javascript">
+	$(document).ready(function()
+		{
+		$("#result").load("result.php",function() {
+		$("#loading").hide();});
+
+	});
+</script>
+
 </html>
 
