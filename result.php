dc8f51239fd7b3f207537a2784d22d6e4e169056
Joost Kingma <joost@facebookgraph.ivi.hva.nl>
2015-04-17 10:37:04 +0200

    added more search options but very early stages

--- result.php
+++ result.php
@@ -46,56 +46,87 @@ try {
 if($session) {
 
   try {
-
-	$request = new FacebookRequest( $session, 'GET','/search?q='.$_POST['search'].'&type=user&fields=id,name,picture.width(100).height(100)&limit=30');
-	$response = $request->execute();
-	// get response
-	$graphObject = $response->getGraphObject()->asArray();
-	for ($i = 0, $count = count($graphObject["data"]); $i < $count; $i = $i+2) {
-		echo '	<div class="row">
-					<div class="col-sm-6">
-						<div class="panel panel-info ">
-							<div class="panel-heading">
-								'.$graphObject["data"][$i]->name.'
-							</div>
-							<div class="panel-body">
-								<div class="row">
-									<div class="col-sm-3">
-										<img src="'.$graphObject["data"][$i]->picture->data->url.'" class="img-rounded" style="width: 100px; height: 100px">
-									</div>
-									<div class="col-sm-3">
-										W.I.P
+	list($type, $q) = explode(' ', $_POST['search']);
+	if($type != "likes") {
+		$request = new FacebookRequest( $session, 'GET','/search?q='.$q.'&type='.$type.'&fields=id,name,picture.width(100).height(100)&limit=30');
+		$response = $request->execute();
+		// get response
+		$graphObject = $response->getGraphObject()->asArray();
+		for ($i = 0, $count = count($graphObject["data"]); $i < $count; $i = $i+2) {
+			echo '	<div class="row">
+						<div class="col-sm-6">
+							<div class="panel panel-info ">
+								<div class="panel-heading">
+									'.$graphObject["data"][$i]->name.'
+								</div>
+								<div class="panel-body">
+									<div class="row">
+										<div class="col-sm-3">
+											<img src="'.$graphObject["data"][$i]->picture->data->url.'" class="img-rounded" style="width: 100px; height: 100px">
+										</div>
+										<div class="col-sm-3">
+											W.I.P
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col-sm-6">
-						<div class="panel panel-info ">
-							<div class="panel-heading">
-								'.$graphObject["data"][$i+1]->name.'
-							</div>
-							<div class="panel-body">
-								<div class="row">
-									<div class="col-sm-3">
-										<img src="'.$graphObject["data"][$i+1]->picture->data->url.'" class="img-rounded" style="width: 100px; height: 100px">
-									</div>
-									<div class="col-sm-3">
-										W.I.P
+						<div class="col-sm-6">
+							<div class="panel panel-info ">
+								<div class="panel-heading">
+									'.$graphObject["data"][$i+1]->name.'
+								</div>
+								<div class="panel-body">
+									<div class="row">
+										<div class="col-sm-3">
+											<img src="'.$graphObject["data"][$i+1]->picture->data->url.'" class="img-rounded" style="width: 100px; height: 100px">
+										</div>
+										<div class="col-sm-3">
+											W.I.P
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>';
+					</div>';
+		} 
+	} else {
+		print_r(fetch_fb_fans($q, 2, 400000));	
 	}	
   } catch(FacebookRequestException $e) {
 
     echo "Exception occured, code: " . $e->getCode();
     echo " with message: " . $e->getMessage();
-
+	
   }   
 
 }
 echo ($_POST['search']);
+
+function fetch_fb_fans($fanpage_name, $no_of_retries = 10, $pause = 500000 /* 500ms */){
+    $ret = array();
+    // get page info from graph
+    $fanpage_data = json_decode(file_get_contents('http://graph.facebook.com/' . $fanpage_name), true);
+    if(empty($fanpage_data['id'])){
+        // invalid fanpage name
+        return $ret;
+    }
+    $matches = array();
+    $url = 'http://www.facebook.com/plugins/fan.php?connections=100&id=' . $fanpage_data['id'];
+    $context = stream_context_create(array('http' => array('header' => 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0')));
+    for($a = 0; $a < $no_of_retries; $a++){
+        $like_html = file_get_contents($url, false, $context);
+        preg_match_all('{href="https?://www\.facebook\.com/([a-zA-Z0-9._-]+)" data-jsid="anchor" target="_blank"}', $like_html, $matches);
+        if(empty($matches[1])){
+            // failed to fetch any fans - convert returning array, cause it might be not empty
+            return array_keys($ret);
+        }else{
+            // merge profiles as array keys so they will stay unique
+            $ret = array_merge($ret, array_flip($matches[1]));
+        }
+        // don't get banned as flooder
+        usleep($pause);
+    }
+    return array_keys($ret);
+}
 ?>
