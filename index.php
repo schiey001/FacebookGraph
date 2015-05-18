bbf53384e15f2c29953833ccb80978ae70f0920c
MMartinezMusterd <miguelmartinez@hotmail.nl>
2015-05-18 13:19:01 +0200

    cleaned up index.php

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
 <?php
-$page = 'index';
+	$page = 'index';
 ?>
-
 <html>
     <?php include('includes/includes_top.php'); ?>
-    <body>
-       
+    <body>   
         <?php include('includes/header.php'); ?>
-
-        <br>
-        <br>
-        <br>
-        
         <div class='container' id='home_text'>
             <h1>Welkom bij Facebook Graph Search</h1>
             <div class="fb-like" data-share="true" data-width="450" data-show-faces="true">
 			</div>
         </div>
-        
-
         <?php include('includes/footer.php'); ?>
-        <?php include('includes/includes_bot.php'); ?>
-
+        <?php include('includes/includes_bottom.php'); ?>
     </body>
 </html>
 
